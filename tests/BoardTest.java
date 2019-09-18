78d643c345e6e717bd90c3dbeb1fd728f1365f49
Tomasz Darmetko <isinlor@gmail.com>
2019-09-18 14:57:25 +0200

    Modify unit tests to increase coverage.

--- tests/BoardTest.java
+++ tests/BoardTest.java
@@ -9,11 +9,13 @@ public class BoardTest extends SimpleUnitTest {
         });
 
         it("allows to retrieve board size", () -> {
+            assertTrue(3 == new Board(3).getSize());
+            assertTrue(10 == new Board(10).getSize());
             assertTrue(15 == new Board(15).getSize());
         });
 
         it("initializes board with empty cells", () -> {
-            Board board = new Board(15);
+            Board board = new Board(3);
             for (int i = 0; i < board.getSize(); i++) {
                 for (int j = 0; j < board.getSize(); j++) {
                     assertTrue(board.getCells()[i][j] == BoardCell.Empty);
