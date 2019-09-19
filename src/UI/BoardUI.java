d03f9c4e6b6daf3ec33cc95dd56463158d39bfc1
Felix <deman.janfelix@gmail.com>
2019-09-19 16:12:11 +0200

    Added mouseclickevent
    int x and y still messy and non scalable,we should fix gridpane display as well

--- src/UI/BoardUI.java
+++ src/UI/BoardUI.java
@@ -8,7 +8,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
