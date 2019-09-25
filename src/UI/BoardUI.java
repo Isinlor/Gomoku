2cb0fcf2c3719774e7b950f7735cf1e7922d2f2c
Thymo ter Doest <thymo@accountingbox.nl>
2019-09-25 12:49:28 +0200

    wip board ui

--- src/UI/BoardUI.java
+++ src/UI/BoardUI.java
@@ -22,57 +22,58 @@ public class BoardUI extends Application{
 
     private BoardCell[][] boardState;
     private int boardSize = 15;
-
+    private Pane root;
+    private Board board;
 
     public void start(Stage primaryStage)
     {
-
-        Pane root = new Pane();
-
+        root = new Pane();
         root.setStyle("-fx-border-color: black;");
-
-
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        Board board = new Board(15);
-        root.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println(event.getSceneX());
-                System.out.println(event.getSceneY());
-                int x =(int) event.getSceneX()/50;
-                int y =(int) event.getSceneY()/50;
-                System.out.println(x);
-                System.out.println(y);
-                board.move(y,x);
-                root.getChildren().add(updateBoard(board));
-            }
-        });
-        root.getChildren().add(updateBoard(board));
-
-
-
-
+        board = new Board(15);
+//        root.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent event) {
+//                System.out.println(event.getSceneX());
+//                System.out.println(event.getSceneY());
+//                int x =(int) event.getSceneX()/50;
+//                int y =(int) event.getSceneY()/50;
+//                System.out.println(x);
+//                System.out.println(y);
+//                board.move(y,x);
+//                root.getChildren().add(updateBoard(board));
+//            }
+//        });
+        updateBoard();
         primaryStage.show();
-
 
     }
 
   
 
-    public Parent updateBoard(Board board){
+    public void updateBoard(){
         boardSize = board.getSize();
         GridPane gameBoard = new GridPane();
         gameBoard.setPrefSize(750, 750);
-
-
-
-        for (int x = 0; x <boardSize; x++) {
+        for (int x = 0; x < boardSize; x++) {
             for (int y = 0; y < boardSize; y++) {
 
                 Rectangle tile = new Rectangle(50, 50);
                 tile.setFill(Color.BURLYWOOD);
                 tile.setStroke(Color.BLACK);
+
+                final int cell_x = x;
+                final int cell_y = y;
+
+                tile.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        System.out.printf("Mouse enetered cell [%d, %d]%n", cell_x, cell_y);
+                        board.move(cell_x,cell_y);
+                        updateBoard();
+                    }
+                });
 
                 switch (board.getCell(x, y)) {
                     case Empty:
@@ -86,14 +87,11 @@ public class BoardUI extends Application{
                         break;
                 }
 
-
-
-
             }
         }
-     
 
-        return gameBoard;
+        root.getChildren().removeAll();
+        root.getChildren().add(gameBoard);
     }
     // Black circle unicode = U+25CF
     // White circle: U+25CB
