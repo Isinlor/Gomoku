a3845be12d49025672765c1273a47c64cfa230d9
Tomasz Darmetko <isinlor@gmail.com>
2019-10-02 12:34:44 +0200

    Set standard for evaluation.

--- src/Contract/Evaluation.java
+++ src/Contract/Evaluation.java
@@ -1,5 +1,11 @@
 package Contract;
 
 public interface Evaluation {
+
+    /**
+     * Evaluate the board from the perspective of the current player.
+     * A good board for the current player should give a high evaluation.
+     */
     double evaluate(ReadableBoard board);
+
 }
--- src/Player/EvaluatePlayer.java
+++ src/Player/EvaluatePlayer.java
@@ -6,21 +6,23 @@ public class EvaluatePlayer implements Player {
 
     private Evaluation evaluation;
 
-    EvaluatePlayer(Evaluation evaluation) {
+    public EvaluatePlayer(Evaluation evaluation) {
         this.evaluation = evaluation;
     }
 
     public Move getMove(ReadableBoard board) {
         Move bestMove = null;
-        double bestEvaluation = Double.MIN_VALUE;
+        double worstEvaluation = Double.MAX_VALUE;
         for (int x = 0; x < board.getSize(); x++) {
             for (int y = 0; y < board.getSize(); y++) {
                 if(board.getCell(x, y) == BoardCell.Empty) {
                     Move move = new Move(x, y);
+                    // because the evaluation is from perspective of the other color
+                    // the best move is the one with the worst evaluation
                     double moveEvaluation = evaluation.evaluate(board.getWithMove(new Move(x, y)));
-                    if(moveEvaluation >= bestEvaluation) {
+                    if(moveEvaluation <= worstEvaluation) {
                         bestMove = move;
-                        bestEvaluation = moveEvaluation;
+                        worstEvaluation = moveEvaluation;
                     }
                 }
             }
