980fb487301c432170bea94afd14a3ab6aa64a31
Tomasz Darmetko <isinlor@gmail.com>
2019-10-02 10:46:58 +0200

    Add evaluate player.

---
+++ src/Player/EvaluatePlayer.java
@@ -0,0 +1,30 @@
+package Player;
+
+import Contract.*;
+
+public class EvaluatePlayer implements PlayerInterface {
+
+    private Evaluation evaluation;
+
+    EvaluatePlayer(Evaluation evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    public Move getMove(ReadableBoard board) {
+        Move bestMove = null;
+        double bestEvaluation = Double.MIN_VALUE;
+        for (int x = 0; x < board.getSize(); x++) {
+            for (int y = 0; y < board.getSize(); y++) {
+                if(board.getCell(x, y) == BoardCell.Empty) {
+                    Move move = new Move(x, y);
+                    double moveEvaluation = evaluation.evaluate(board.getWithMove(new Move(x, y)));
+                    if(moveEvaluation >= bestEvaluation) {
+                        bestMove = move;
+                        bestEvaluation = moveEvaluation;
+                    }
+                }
+            }
+        }
+        return bestMove;
+    }
+}
