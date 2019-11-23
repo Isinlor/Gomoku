508fb674e921d1eeab8fe31513b4910d10f6eafe
Tomasz Darmetko <isinlor@gmail.com>
2019-11-23 15:10:53 +0100

    Add new move selector test.

--- tests/MoveSelectorTest.java
+++ tests/MoveSelectorTest.java
@@ -1,6 +1,7 @@
 import Board.Helpers.ApproximateMoveSelector;
 import Board.SimpleBoard;
 import Contract.BoardCell;
+import Contract.Move;
 
 public class MoveSelectorTest extends SimpleUnitTest {
 
@@ -41,6 +42,31 @@ public class MoveSelectorTest extends SimpleUnitTest {
             SimpleBoard board = new SimpleBoard(boardState);
 
             assertEqual(new ApproximateMoveSelector().getMoves(board).size(), 12);
+
+        });
+
+        it("returns all moves following made moves", () -> {
+
+            SimpleBoard board = new SimpleBoard(5);
+
+            board.move(new Move(0, 0));
+            assertEqual(new ApproximateMoveSelector().getMoves(board).size(), 3);
+
+            board.move(new Move(4, 4));
+            assertEqual(new ApproximateMoveSelector().getMoves(board).size(), 6);
+
+            board.revertMove(new Move(0, 0));
+            assertEqual(new ApproximateMoveSelector().getMoves(board).size(), 3);
+
+            board.move(new Move(2, 2));
+            assertEqual(new ApproximateMoveSelector().getMoves(board).size(), 10);
+
+            SimpleBoard boardCopy = new SimpleBoard(board);
+            assertEqual(new ApproximateMoveSelector().getMoves(boardCopy).size(), 10);
+
+            boardCopy.resetBoard();
+            assertEqual(new ApproximateMoveSelector().getMoves(boardCopy).size(), 25);
+            assertEqual(new ApproximateMoveSelector().getMoves(board).size(), 10);
 
         });
 
