ee176bd72c24c437f7eb8f8804ab8d8fa58fb9e4
Tomasz Darmetko <isinlor@gmail.com>
2020-01-10 13:54:58 +0100

    Add simplest possible NN.

---
+++ src/main/java/NeuralNetwork/XOR_NN.java
@@ -0,0 +1,88 @@
+package NeuralNetwork;
+
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.learning.config.Nadam;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+public class XOR_NN {
+    public static void main(String[] args) {
+
+        // XOR
+        // 0 0 | 0
+        // 0 1 | 1
+        // 1 0 | 1
+        // 1 1 | 0
+
+        int numSamples = 4;
+        int sampleLength = 2;
+        int labelLength = 1;
+
+        INDArray samples = Nd4j.create(
+            new float[]{
+                0, 0,
+                0, 1,
+                1, 0,
+                1, 1
+            },
+            new int[]{numSamples, sampleLength}
+        );
+
+        INDArray labels = Nd4j.create(
+            new float[]{
+                0, 1, 1, 0
+            },
+            new int[]{numSamples, labelLength}
+        );
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .seed(123) //include a random seed for reproducibility
+            .activation(Activation.RELU)
+            .weightInit(WeightInit.XAVIER)
+            .updater(new Nadam())
+            .list()
+            .layer(new DenseLayer.Builder()
+                .nIn(2)
+                .nOut(10)
+                .build())
+            .layer(new DenseLayer.Builder()
+                .nIn(10)
+                .nOut(10)
+                .build())
+            .layer(new OutputLayer.Builder(LossFunctions.LossFunction.SQUARED_LOSS)
+                .activation(Activation.SIGMOID)
+                .nOut(1)
+                .build())
+            .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+        DataSet ds = new DataSet(samples, labels);
+        for (int epoch = 0; epoch < 1000; epoch++)
+            model.fit(ds);
+
+        model.setListeners(new ScoreIterationListener(1));
+
+        System.out.println(model.output(
+            Nd4j.create(
+                new float[]{
+                    0, 0,
+                    0, 1,
+                    1, 0,
+                    1, 1
+                },
+                new int[]{numSamples, sampleLength}
+            )
+        ));
+    }
+}
