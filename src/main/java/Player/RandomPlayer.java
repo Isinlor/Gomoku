dacd8d7ed2baf4ed1d2b0b48e911b94d17d9f77f
Tomasz Darmetko <isinlor@gmail.com>
2020-01-14 09:42:25 +0000

    Fix random.

--- src/main/java/Player/RandomPlayer.java
+++ src/main/java/Player/RandomPlayer.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 public class RandomPlayer implements Player {
 
+    public static Random random = new Random();
+
     private MoveSelector moveSelector;
 
     public RandomPlayer(MoveSelector moveSelector) {
@@ -15,8 +17,7 @@ public class RandomPlayer implements Player {
     public Move getMove(ReadableBoard board) {
 
         Move move;
-        Random random = new Random();
-
+        
         List<Move> moves = new ArrayList<>(moveSelector.getMoves(board));
 
         return moves.get(random.nextInt(moves.size()));
